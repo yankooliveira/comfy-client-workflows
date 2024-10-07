3622941f99c6f843cc7228cc04bf7e34f3341ae8
Yanko <yankooliveira@gmail.com>
2024-10-07 10:57:42 +0100

    Adds a new "Workflow" class that allows reasoning about nodes by name.
    Also splits "Prompt" into "Prompt" and "PromptNode".

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+import { ComfyUIClient } from "./client";
+
 export interface NodeInfo {
   class_type: string;
   inputs: {
@@ -6,13 +8,15 @@ export interface NodeInfo {
   };
 }
 
+export interface PromptNode {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  inputs: Record<string, any>;
+  class_type: string;
+  _meta: any;
+};
+
 export interface Prompt {
-  [nodeId: string]: {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    inputs: Record<string, any>;
-    class_type: string;
-    _meta: any;
-  };
+  [nodeId: string]: PromptNode;
 }
 
 export interface ComfyUIError {
@@ -149,3 +153,103 @@ export type FolderName =
   | 'upscale_models'
   | 'custom_nodes'
   | 'hypernetworks';
+
+  export class ComfyWorkflow {
+    RawPrompt: Prompt;
+    InputNodePrefix: string;
+    OutputNodePrefix: string;
+  
+    InputNodes : Record<string, PromptNode>;
+    OutputNodeIdsByName : Record<string, string>;
+  
+    constructor(prompt: Prompt, inputNodePrefix: string = "INPUT_", outputNodePrefix: string = "OUTPUT_") {
+      this.RawPrompt = prompt;
+      this.InputNodePrefix = inputNodePrefix;
+      this.OutputNodePrefix = outputNodePrefix;
+  
+      this.InputNodes = {};
+      this.OutputNodeIdsByName = {};
+  
+      // Cache all "input" and "output" nodes.
+      for (const nodeId in this.RawPrompt) {
+        const node = this.RawPrompt[nodeId];
+        if (node._meta) {
+          if(ComfyWorkflow.nodeMatchesQuery(node._meta.title, inputNodePrefix, false, false)) {
+            this.InputNodes[node._meta.title.replace(inputNodePrefix, "")] = node;
+          } else if(ComfyWorkflow.nodeMatchesQuery(node._meta.title, outputNodePrefix, false, false)) {
+            this.OutputNodeIdsByName[node._meta.title.replace(outputNodePrefix, "")] = nodeId;
+          }
+        }
+      }
+    }
+  
+    public async getImages(client: ComfyUIClient) : Promise<Record<string, ImageContainer[]>> {
+      const finalCollection: Record<string, ImageContainer[]> = {};
+      const images = await client.getImages(this.RawPrompt);
+
+      for(const outputName in this.OutputNodeIdsByName) {
+        const result = this.getImagesFromOutput(outputName, images);
+        if(result) {
+          finalCollection[outputName] = result;
+        }
+      }
+
+      return finalCollection;
+    }
+
+    public getImagesFromOutput(nodeName: string, response: ImagesResponse): ImageContainer[] | undefined {
+      return response[this.OutputNodeIdsByName[nodeName]];
+    }
+
+    public getNodeByName(nodeName: string, matchExact: boolean = true, matchCase: boolean = false): PromptNode | undefined {
+      const nodeId = this.getNodeIdByName(nodeName, matchExact, matchCase);
+      if(nodeId) {
+        return this.RawPrompt[nodeId];
+      }
+      return undefined;
+    }
+  
+    public getNodeIdByName(nodeName: string, matchExact: boolean = true, matchCase: boolean = false): string | undefined {
+      const result = this.getNodeIdsByNodeName(nodeName, matchExact, matchCase);
+      if (result) {
+        if (result.length === 0) {
+          return undefined;
+        }
+        if (result.length > 1) {
+          console.log(`[ComfyUI Workflow Wrapper] More than one node matches name ${nodeName}. Returning first occurrence`);
+        }
+        return result[0]
+      }
+    }
+  
+    public getNodeIdsByNodeName(nodeName: string, matchExact: boolean = true, matchCase: boolean = false): string[] | undefined {
+      const matchingNodes: string[] = [];
+  
+      for (const nodeId in this.RawPrompt) {
+        const node = this.RawPrompt[nodeId];
+        if (node._meta) {
+          if (ComfyWorkflow.nodeMatchesQuery(node?._meta.title, nodeName, matchExact, matchCase)) {
+            matchingNodes.push(nodeId);
+          }
+        }
+      }
+  
+      return matchingNodes;
+    }
+  
+    private static nodeMatchesQuery(metadataTitle:string, desiredName: string, matchExact: boolean, matchCase: boolean) {
+      if(!metadataTitle) {
+        return false;
+      }
+  
+      var nameA = metadataTitle;
+      var nameB = desiredName;
+  
+      if(!matchCase) {
+        nameA = nameA.toLowerCase();
+        nameB = nameB.toLowerCase();
+      }
+  
+      return (matchExact ? nameA === nameB : nameA.includes(nameB));
+    }
+  }
